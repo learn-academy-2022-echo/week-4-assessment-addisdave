3226dfa23088a7ad705f1743cbcabb714604cadb
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-08-29 02:42:33 -0700

    week-4- assignment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
 
+
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -10,41 +12,190 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+// a describe method that lists the name of the function OR naming of the particular test.
+
+
+// describe("hello", () => {
+//     // a test/it method, nested within the describe block, that in plain words, describes that the function does.
+//     it("returns a string that says hi", () => {
+//       //an expect method, nested within the test block, calling on the hello() function, followed by the .toEqual() matcher that checks the expected output of the function return.
+//       expect(hello()).toEqual("hi");
+//     });
+//   });
 
 // --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
 
+
 // a) Create a test with an expect statement using the variable provided. 
+
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-const colors1 = ["purple", "blue", "green", "yellow", "pink"]
-// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
-const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
-// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+describe("my_Shuffler", () => {
+    
+    it("takes in an array, removes the first item from the array and shuffles the remaining content.", () => {
 
+        const colors1 = ["purple", "blue", "green", "yellow", "pink"]
+        // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+        const colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+        // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]    
+
+     expect(my_Shuffler(colors1)).not.toEqual(["yellow", "blue", "pink", "green"]);
+     expect(my_Shuffler(colors2)).not.toEqual(["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]);
+
+    });
+  });
+
+//    ****** GOOD FAIL *****
+
+//   FAIL  ./code-challenges.test.js
+//   my_Shuffler
+//     ✕ takes in an array, removes the first item from the array and shuffles the remaining content. (1 ms)
+
+//   ● my_Shuffler › takes in an array, removes the first item from the array and shuffles the remaining content.
+
+//     ReferenceError: my_Shuffler is not defined
 
 // b) Create the function that makes the test pass.
+
+// PSEUDOCODE:
+// Create a function named my_Shuffler.
+// Inputs are array of colors named colors1 and colors2.
+// Outputs should show an array that removes the first item from the array and shuffles the remaining content.
+// Use built-in methods .pop, .length and  to shuffle and remove items in array.
+
+ const my_Shuffler = (array) => {
+    let shuffler1 = array.pop()
+    let shaffler2 = (arr) => {
+    let thisIndex = arr.lenth, randomIndex;
+    while (thisIndex) {
+        randomIndex = math.floor(math.random() * thisIndex)
+    ;
+    thisIndex;
+    [arr[thisIndex], arr[randomIndex]]
+= [arr[randomIndex], arr[thisIndex]];   
+     }
+     return arr
+    }
+   shaffler2(shuffler1)
+}
+
+
+// const my_Shuffler = (array) => {
+//     let shuffler1 = array.shift();
+//     let shuffler2 =(array) => {
+//         let currentIndex = array.shuffle()
+//     }
+//        return shuffler2
+// }
+// my_Shuffler
+
+// PASS  ./code-challenges.test.js
+// my_Shuffler
+//   ✓ takes in an array, removes the first item from the array and shuffles the remaining content. (2 ms)
+
 
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the net total of votes.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const votes1 = {upVotes: 13, downVotes: 2}
-// Expected output: 11
-const votes2 = {upVotes: 2, downVotes: 33}
-// Expected output: -31
 
+
+describe("my_Votter", () => {
+    
+    it("takes in an object that contains up votes and down votes and returns the net total of votes.", () => {
+
+        const votes1 = {upVotes: 13, downVotes: 2}
+        // Expected output: 11
+        const votes2 = {upVotes: 2, downVotes: 33}
+        // Expected output: -31  
+
+     expect(my_Votter(votes1)).toEqual(11);
+     expect(my_Votter(votes2)).toEqual(-31);
+
+    });
+  });
+
+
+//             ***** GOOD FAIL ****
 
 // b) Create the function that makes the test pass.
+
+// PSUDOCODE:
+// Create a function called my_Votter.
+// Iputs are objects of key values with down and up upvotes total.
+// Returns are net total votes
+// Use Ruby built-in method in to function. 
+
+const my_Votter = (object) => {
+    let total_votes = (object.upVotes - object.downVotes);
+    return total_votes
+}
+
+// PASS  ./code-challenges.test.js
+// my_Votter
+//   ✓ takes in an object that contains up votes and down votes and returns the net total of votes. (2 ms)
+
 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
 
-const dataArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+//const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+//const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+//Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+
+ describe("my_Returner", () => {
+    
+    it("takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.", () => {
+
+       const dataArray1 = ["array", "object", "number", "string", "Boolean"]
+       const dataArray2 = ["string", "null", "Boolean", "string", "undefined"]
+       //Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+
+     expect(my_Returner(dataArray1, dataArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"]);
+     
+
+    });
+  });
+
+
+//          **** GOOD FAIL ****    
+
+//   FAIL  ./code-challenges.test.js
+//   my_Returner
+//     ✕ takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments. (1 ms)
+
+//   ● my_Returner › takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+
+//     ReferenceError: my_Returner is not defined
 
 
 // b) Create the function that makes the test pass.
+
+// PSUEDOCODE:
+// Write a function called my_Returner
+// Inputs are two arrays dataArray1&2
+// Output is one array wthat combines the two by omitting duplicates.
+// Use built-in methods of 
+// .concat() to create a new array by combining element in array, 
+// .filter() to callback function once for each element in array and constructs a new array,
+// .indexOf() to compare an elemt to elements in array using strict equality. If greater than 
+
+const my_Returner = (array1, array2) => {
+    let dataArray1 = [];
+    let dataArray2 = dataArray1.concat(dataArray1)
+    let myArray2 = () => {
+       (myArray1.filter(), dataArray1.indexOf() < 0)
+    }
+
+    
+  //omit_Duplicator = secondArray.filter((item) => dataArray1.indexOf(item) < 0))
+
+    return myArray2
+
+}
+//****** MY FUNCTION FAILED */ CAN USE (...array1, array2) to join them or use concat. How to remove the duplicates?
+
+//dataArray1.concat(dataArray2.filter((item) => dataArray1.indexOf(item) < 0))
--- interview-questions.md
+++ interview-questions.md
@@ -8,42 +8,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: OOP use classes to create objects. FP use function to do a particular task.
 
-Researched answer:
+Researched answer: OOP, data is directely stored in objects where as FP, data is only transfered using functions.
 
 2. What is the difference between a Float and an Integer in Ruby?
 
-Your answer:
+Your answer: Float icludes decimals but integer does not.
 
-Researched answer:
+Researched answer: float is numbers containing decimal point whereas integers in Ruby that doesn't have decimals. 
 
 3. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: returns last output of a function without any return command.
 
-Researched answer:
+Researched answer: It a return that if the return is expressed last furing an excution, no need to return the keyword. 
 
-4. What is a block in Ruby?
+4. What is a block in Ruby? 
 
-Your answer:
+Your answer: chunk of codes go between do and end. 
 
-Researched answer:
+Researched answer: Blocks are anonymous function passed to methods inclosed between  do   end statement or in {} that can 
+take multiple arguments defined between ||. For example, [1 ,2].each    {|num|                puts num}
+                                                                <!-- block argument        block body -->
+
+                                  3.times do
+                                        puts "Echo"
+                                    end
+// => outputs is three prints of "Echo"  3.times is the method call.
+// or same out put with the currly brackets.     3.times() {puts "Echo"}
+
 
 5. What is an instance variable in Ruby?
 
-Your answer:
+Your answer: it is a way of creating a new variable so it can be passed to new function. 
 
-Researched answer:
+Researched answer: it is a variable tha belongs to a specific inctance of class which its value stored its unique
+value privately within the class. Ruby uses @ to a variable name to indicate instances of variables. 
 
 ## Looking Ahead: Terms for Next Week
 
-1. PostgreSQL:
+1. PostgreSQL: it is an object-relational database management system. 
 
-2. Ruby on Rails:
+2. Ruby on Rails: it is a server-side web application frame work written in Ruby.
 
-3. ORM:
+3. ORM: Object Rational Mapperprovides an object oriented layer in b/n rational database and OOP without writing SQL. 
 
-4. Active Record:
+4. Active Record: is interface between a Rails application and the database.
 
-5. Migrations:
+5. Migrations: It is used to modify the shape of an existing datatabase. (from the syllabus)
+file generated through rails command which allow us to make changes into database. 
